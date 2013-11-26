b6ccfe8d08657abd0b551b40a51a42c01c550d15
Goran <gorancolovic10@gmail.com>
2013-11-26 12:25:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Program to divide numbers
+
+gitproj demo
---
+++ src/divide/Div.java
@@ -0,0 +1,21 @@
+package divide;
+
+class Divide{
+	
+	private int a = 0;
+	
+	public int Dividenum(int x, int y){
+		
+		return a = x / y;
+	}
+}
+
+public class Div {
+	
+	public static void main(String[] args){
+		
+		Divide d = new Divide();
+		System.out.println(d.Dividenum(10, 5));
+		System.out.println(d.Dividenum(15, 5));
+	}
+}
